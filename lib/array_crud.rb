413d41cc2ddc25570c5886021a685cb94e2a6ede
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 01:15:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array=["I", "am", "learning", "about"]
+  create_an_array=["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(create_an_array, element)
@@ -22,9 +22,9 @@ def remove_element_from_start_of_array(create_an_empty_array)
   create_an_empty_array.shift
 end
 
-def retrieve_element_from_index(create_an_empty_array, 1)
+def retrieve_element_from_index(create_an_empty_array, 2)
   create_an_array=["I", "am", "learning", "about"]
-  create_an_empty_array[1]
+  create_an_empty_array[2]
 end
 
 def retrieve_first_element_from_array(array)
