b3d314bfe28d85ec069e7ec9f2ca02eb5a8f0db1
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 01:16:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array=["wow", "I", "am", "really", "learning"]
+  create_an_array=[ "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(create_an_array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(create_an_empty_array)
 end
 
 def retrieve_element_from_index(array, element)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
