a75c941e7cc9be921765f5312e2dabc4431a84ce
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 00:50:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,8 @@ def create_an_array
   ["violet","indigo","magenta","pink"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(create_an_array, "sea green")
+  <<"sea green"
 end
 
 def add_element_to_start_of_array(array, element)
