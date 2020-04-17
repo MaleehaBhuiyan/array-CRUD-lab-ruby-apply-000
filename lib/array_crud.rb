0db1c7a7a11b9a2a32aca6288904780653ea167a
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 01:29:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_start_of_array(create_an_empty_array)
   create_an_empty_array.shift
 end
 
-def retrieve_element_from_index(create_an_empty_array, [2])
+def retrieve_element_from_index(create_an_empty_array, 2)
 end
 
 def retrieve_first_element_from_array(array)
