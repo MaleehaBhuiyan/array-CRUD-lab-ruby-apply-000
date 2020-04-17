444aac6dc1893e07718e21d8be482583337f477f
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 01:03:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   create_an_array=["Hi", "my", "name", "is"]
 end
 
-def add_element_to_end_of_array(array,element)
-
+def add_element_to_end_of_array(create_an_array,"is")
+  create_an_array<<"Maleeha"
 end
 
 def add_element_to_start_of_array(array, element)
