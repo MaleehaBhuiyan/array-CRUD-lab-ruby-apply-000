c55381a5be77a9e508a520e70356cb64c97c8c11
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 00:49:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,7 +2,8 @@ def create_an_empty_array
   []
 end
 
-def create_an_array = ["violet","indigo","magenta","pink"]
+def create_an_array 
+  ["violet","indigo","magenta","pink"]
 end
 
 def add_element_to_end_of_array(array, element)
